6b9e5af6088ebc63321da83566a08344f83c6ac5
Tuomo Virolainen <tuomo.virolainen@kapsi.fi>
2017-05-13 19:58:37 +0300

    saner scraping

--- src/kaupparekisteri/core.clj
+++ src/kaupparekisteri/core.clj
@@ -1,6 +1,7 @@
 (ns kaupparekisteri.core
   (:require [clj-http.client :as client]
             [clojure.data.json :as json]
+            [clojure.string :as s]
             [clojure.data.csv :as csv]
             [clj-time.core :as t]
             [clj-time.format :as f]
@@ -8,28 +9,45 @@
             [monger.collection :as mc]
             [clj-time.local :as l]))
 
-(def url1 "http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=10&resultsFrom=10&companyRegistrationFrom=1800-01-01")
+(def time-formatter
+  (f/formatter "yyyy-MM-dd"))
 
-(def url "http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=1000&companyRegistrationFrom=1800-01-01&resultsFrom=")
+(defn previousdate 
+ "Returns a date object for a date [days] prior to present."
+ [days]
+  (-> days t/days t/ago))
 
-(defn query [url]
-  (-> url client/get :body json/read-str clojure.walk/keywordize-keys))
-
-(defn extract-names [res]
-  (->> res :results (map :name)))
+(def end-date
+  (t/date-time 1900 1 1))
 
 (defn set-id [item]
   (clojure.set/rename-keys item {:businessId :_id}))
 
+(defn parse-response [res]
+  (->> res :body json/read-str clojure.walk/keywordize-keys :results (map set-id)))
+
+(defn parse-query
+  "Takes two integers, which denote the time range for the query as days from the present.
+  Returns a parsed query string, which fetches results from a five-day range."
+  [start]
+  (let [formatter (partial f/unparse time-formatter)
+        startdate (->> start previousdate formatter)
+        lastdate (->> (- start 5) previousdate formatter)]
+    (str "http://avoindata.prh.fi/bis/v1?totalResults=true&maxResults=1000&companyRegistrationFrom=" startdate
+         "&companyRegistrationTo=" lastdate)))
+
 (defn fetch-all []
   (let [conn (mg/connect)
-        db (mg/get-db conn "kaupparekisteri")]
-    (loop [page 0]
-      (let [data (->> (str url page) query :results (map set-id))]
-        (if (empty? data)
-          (println "Done.")
+        db (mg/get-db conn "kauppa")]
+    (loop [begin 5]
+      (let [date (previousdate begin)
+            data (client/get (parse-query begin))
+            resultset (parse-response data)
+            dates (re-seq #"\d{4}-\d{2}-\d{2}" (parse-query begin))]
+        (if (t/after? end-date date)
+          "Done."
           (do
-            (mc/insert-batch db "data" data)
-            (println (clojure.string/join "\n" (map :name data)))
-            (Thread/sleep 10000)
-            (recur (+ page 1000))))))))
+            (mc/insert-batch db "data" resultset)
+            (println (str "Fetched " (count resultset) " results (" (first dates) " - " (last dates)")"))
+            (Thread/sleep 3000)
+            (recur (+ begin 5))))))))
